07a43ab3ab5883c6854755b2f6c608a230e7340b
Sanjay Saini <sanjaysaini2000@hotmail.com>
2020-01-12 22:10:49 +0530

    implemented Put api call code

--- Controller/CustomerController.js
+++ Controller/CustomerController.js
@@ -5,6 +5,8 @@ const router = express.Router();
 
 router.use(express.json());
 
+let ObjectId = require("mongoose").Types.ObjectId;
+
 router.get("/test", (req, resp) => {
   resp.send("hello world from customer...");
 });
@@ -13,16 +15,62 @@ router.get("/test/:id", (req, resp) => {
   resp.send(`hello world from customer id ${req.params.id}`);
 });
 
+//GET http://localhost:3000/customers
 router.get("/", (req, resp) => {
   Customer.find((err, docs) => {
     if (err)
       console.log(
-        "error in getting customers..." + JSON.stringify(err, undefined, 2)
+        "Error while getting customers..." + JSON.stringify(err, undefined, 2)
       );
     else resp.send(docs);
   });
 });
 
+//GET http://localhost:3000/customers/id
+router.get("/:id", (req, resp) => {
+  let customerId = req.params.id;
+  if (!ObjectId.isValid(customerId))
+    return resp.status(400).send(`Customer not found for id :${customerId}`);
+
+  Customer.findById(customerId, (err, docs) => {
+    if (err)
+      console.log(
+        "Error while getting customers..." + JSON.stringify(err, undefined, 2)
+      );
+    else resp.send(docs);
+  });
+});
+
+//PUT http://localhost:3000/customers/id
+router.put("/:id", (req, resp) => {
+  let customerId = req.params.id;
+  if (!ObjectId.isValid(customerId))
+    return resp.status(400).send(`Customer not found for id :${customerId}`);
+
+  let cust = new Customer({
+    first_name: req.body.first_name,
+    last_name: req.body.last_name,
+    gender: req.body.gender,
+    age: req.body.age,
+    email: req.body.email
+  });
+
+  Customer.findByIdAndUpdate(
+    customerId,
+    { $set: cust },
+    { new: true },
+    (err, doc) => {
+      if (err)
+        console.log(
+          "Error while uppdating customers..." +
+            JSON.stringify(err, undefined, 2)
+        );
+      else resp.status(200).send(doc);
+    }
+  );
+});
+
+//POST http://localhost:3000/customers
 router.post("/", (req, resp) => {
   let cust = new Customer({
     first_name: req.body.first_name,
